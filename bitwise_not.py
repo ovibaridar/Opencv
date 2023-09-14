263a2b3abccf8e10cc9237035a871d00308627d9
ovibaridar <ovibaridar06@gmail.com>
2023-09-15 03:45:10 +0600

    bitwise_not/negative

---
+++ bitwise_not.py
@@ -0,0 +1,12 @@
+import cv2 as cv
+
+path = "img/ar.jpg"
+img = cv.imread(path)
+# img = cv.bitwise_not(img)
+img = cv.bitwise_not(img)
+
+img = cv.resize(img, (0, 0), fx=.5, fy=.5)
+
+cv.imshow("Image", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
