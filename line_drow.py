8aeb843fbbd3e7708e655277b369f8d2877c9f78
ovibaridar <ovibaridar06@gmail.com>
2023-09-24 00:21:55 +0600

    line

--- line_drow.py
+++ line_drow.py
@@ -8,7 +8,7 @@ Height = a.shape[0]
 print("Height", Height)
 weight = a.shape[1]
 print("Weight", weight)
-
+# a = cv.line(a, (weight, Height), (weight, Height), (color), thickness)
 a = cv.line(a, (0, 0), (800, 600), (0, 255, 255), 10)
 a = cv.line(a, (0, Height), (800, 600), (0, 255, 255), 10)
 
