15763df9b7b0e91a32ae41c647a94dadc9d8a351
ovibaridar <ovibaridar06@gmail.com>
2023-09-24 00:12:23 +0600

    line

---
+++ line_drow.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+
+path = "img/A.jpeg"
+
+a = cv.imread(path)
+
+Height = a.shape[0]
+print("Height", Height)
+weight = a.shape[1]
+print("Weight", weight)
+
+a = cv.line(a, (0, 0), (800, 600), (0, 255, 255), 10)
+a = cv.line(a, (0, Height), (800, 600), (0, 255, 255), 10)
+
+cv.imshow("aa0", a)
+cv.waitKey(0)
+cv.destroyAllWindows()
